3b8dc708f51831690a6d2e139afa296363053193
Suqin <suqin.zheng@thoughtworks.com>
2020-11-11 19:55:18 +0800

    [Suqin] feat: finish practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
-export { parseData };
+const parseData = ({data, column}) => {
+    let output = [];
+
+    data.map(data => {
+        let member = {};
+        column.forEach(column => {
+            member[column.name] = data.shift();
+        });
+        output.push(member);
+    })
+    return output;
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+    let itemsCopy = [...items];
+    sections.sort((former, latter) => latter.index - former.index)
+        .forEach(section => {
+            itemsCopy.splice(section.index, 0, section.content)
+        });
+    return itemsCopy;
+}
+export {inject};
